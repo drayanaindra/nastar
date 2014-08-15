f49d5b464991ea4ca10ca7ec0fe70e431fd35dc1
drayanaindra <masepindrayana@gmail.com>
2014-08-16 02:00:12 +0700

    push all project

---
+++ README.md
@@ -0,0 +1,38 @@
+## nastar
+
+nastar is Web Content Management System or CMS. Based on [django](https://www.djangoproject.com/) and [django-cms](https://www.django-cms.org/en/) this project is instant django-cms for develop and deployment, using twoscoop method.
+
+
+#### How To Install
+
+Create virtualenv:
+
+    $ virtualenv nastar
+    $ source nastar/bin/activate
+
+____________________________________________
+Clone this project:
+
+    $ git clone https://github.com/drayanaindra/nastar.git
+
+____________________________________________
+Or when you has been forking this project:
+
+    $ git clone git@github.com:drayanaindra/nastar.git
+    
+____________________________________________
+Install requirements:
+
+    $ cd nastar/requirements
+    $ pip install -r base.txt
+    
+____________________________________________
+Run nastar on your local:
+
+    $ ./manage.py runserver --settings=nastar.settings.local
+
+and open in your browser http://localhost:8000
+____________________________________________
+
+Powered : [TANGKIL Lab](http://www.tangkil.com)
+Question : [@drayanaindra](https://www.twitter.com/drayanaindra)
---
+++ nastar/nastar/settings/base.py
@@ -0,0 +1,169 @@
+"""
+Django settings for nastar project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from os.path import abspath, basename, dirname, join, normpath
+from sys import path
+
+gettext = lambda s: s
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_PATH = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+
+DJANGO_ROOT = dirname(dirname(abspath(__file__)))
+
+SITE_ROOT = dirname(DJANGO_ROOT)
+SITE_NAME = basename(DJANGO_ROOT)
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'lw9wfzd@$j#o)gdct7j80i1(t98%xkax6d@jv9p9_qohv)#3e1'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = (
+    'cms',  # django CMS itself
+    'mptt',  # utilities for implementing a modified pre-order traversal tree
+    'menus',  # helper for model independent hierarchical website navigation
+    'south',  # intelligent schema and data migrations
+    'sekizai',  # for javascript and css management
+    'djangocms_admin_style',  # for the admin skin. You **must** add 'djangocms_admin_style' in the list **before** 'django.contrib.admin'.
+    'djangocms_file',
+    'djangocms_flash',
+    'djangocms_googlemap',
+    'djangocms_inherit',
+    'djangocms_picture',
+    'djangocms_teaser',
+    'djangocms_video',
+    'djangocms_link',
+    'djangocms_snippet',
+    'djangocms_text_ckeditor',  # note this needs to be above the 'cms' entry
+)
+
+LOCAL_APPS = (
+
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'sekizai.context_processors.sekizai',
+    'cms.context_processors.cms_settings',
+)
+
+ROOT_URLCONF = 'nastar.urls'
+
+WSGI_APPLICATION = 'nastar.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+SITE_ID = 1
+
+LANGUAGES = [
+    ('en', 'English'),
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    normpath(join('static')),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+# media setting
+MEDIA_ROOT = normpath(join('media'))
+MEDIA_URL = "/media/"
+
+# templates 
+TEMPLATE_DIRS = (
+    normpath(join('templates')),
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+# cms settings
+CMS_TEMPLATES = (
+    ('nastar/templates/templates_1.html', 'Template One'),
+    ('nastar/templates/templates_2.html', 'Template Two'),
+)
